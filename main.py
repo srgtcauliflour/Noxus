109f1953057a425f203eea1ad69baf41c8eb6ebc
Vinicius R <rodriguessv30@gmail.com>
2018-01-02 17:09:44 -0300

    :octocat:

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from Noxus import computer
+from Noxus import cpu
 import sys
 
 params = sys.argv
@@ -12,8 +13,11 @@ if len(params) > 1:
         print("Node de Rede: ", computer.name())
     elif params[1] == "distro":
         print("Distribuicao: ", computer.distro())
-    elif params[1] == "processor", or params[1] == "-p":
+    elif params[1] == "processor" or params[1] == "-p":
         print("processador: ", computer.cpu())
+        print("Velocidade: ", cpu.freq(), "GHz")
+        print("Cores: ", cpu.cores())
+        print("Cores Fisicos: ",cpu.phycores())
     elif params[1] == "arc":
         print("Arquitetura da Maquina")
     else:
