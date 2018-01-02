d6643312ff0afa9d1aaff8287ac547a6db1eb6cb
Vinicius R <rodriguessv30@gmail.com>
2018-01-02 15:47:54 -0300

    update all

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from Noxus import computer
+import sys
 
-print(computer.os(),computer.osVersion())
-print(computer.cpu())
-print(computer.arc())
-print(computer.name())
+params = sys.argv
+
+
+if len(params) > 1:
+    if params[1] =="os" or params[1] == "system":
+        print("Sistema Operacional: ", computer.os())
+        print("versao: ", computer.osVersion())
+    elif params[1] == "name":
+        print("Node de Rede: ", computer.name())
+    elif params[1] == "distro":
+        print("Distribuicao: ", computer.distro())
+    elif params[1] == "processor", or params[1] == "-p":
+        print("processador: ", computer.cpu())
+    elif params[1] == "arc":
+        print("Arquitetura da Maquina")
+    else:
+        print("Parametro desconhecido!")
+else:
+    print("Sistema Operacional: ", computer.os())
