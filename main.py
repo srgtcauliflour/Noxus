08e2911e6f5523b2fda2c116c4191745096abd97
Vinicius R <rodriguessv30@gmail.com>
2018-01-02 17:32:02 -0300

    update main

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import sys
 
 params = sys.argv
 
-
 if len(params) > 1:
     if params[1] =="os" or params[1] == "system":
         print("Sistema Operacional: ", computer.os())
@@ -14,12 +13,18 @@ if len(params) > 1:
     elif params[1] == "distro":
         print("Distribuicao: ", computer.distro())
     elif params[1] == "processor" or params[1] == "-p":
-        print("processador: ", computer.cpu())
-        print("Velocidade: ", cpu.freq(), "GHz")
-        print("Cores: ", cpu.cores())
-        print("Cores Fisicos: ",cpu.phycores())
+        if len(params) == 4 and params[2] == "percentage" and int(params[3]) >= 1:
+            for x in range (int (params[3])):
+              print("Consumindo: ", cpu.percentage().user + cpu.percentage().system, "%")
+              print("Livre: ", cpu.percentage().idle, "%")
+              print("__________________________________")
+        else:
+          print("processador: ", computer.cpu())
+          print("Velocidade: ", cpu.freq(), "GHz")
+          print("Cores: ", cpu.cores())
+          print("Cores Fisicos: ",cpu.phycores())
     elif params[1] == "arc":
-        print("Arquitetura da Maquina")
+        print("Arquitetura da Maquina", computer.arc())
     else:
         print("Parametro desconhecido!")
 else:
